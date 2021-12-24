ab70fe4216dd6a8f4ba5e37afc0741e71c65bb5b
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-12-24 10:27:31 +0900

    find missing Element - in progress

---
+++ PermMissingElem.js
@@ -0,0 +1,32 @@
+/*
+test result
+example
+example test✔OK
+expand allCorrectness tests
+▶empty_and_single
+empty list and single element✘RUNTIME ERROR
+tested program terminated with exit code 1
+▶missing_first_or_last
+the first or the last element is missing✘RUNTIME ERROR
+tested program terminated with exit code 1
+▶single
+single element✘RUNTIME ERROR
+tested program terminated with exit code 1
+▶double
+two elements✘RUNTIME ERROR
+tested program terminated with exit code 1
+▶simple
+simple test✔OK
+expand allPerformance tests
+▶medium1
+medium test, length = ~10,000✔OK
+▶medium2
+medium test, length = ~10,000✔OK
+▶large_range
+range sequence, length = ~100,000✘RUNTIME ERROR
+tested program terminated with exit code 1
+▶large1
+large test, length = ~100,000✔OK
+▶large2
+large test, length = ~100,000✔OK
+*/
